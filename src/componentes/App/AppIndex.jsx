6fd1a651bad9dae3566904461f618477e5daf936
XFernando43 <U20171E997@upc.edu.pe>
2023-11-07 01:02:34 -0500

    funciona pero no me convence

--- src/componentes/App/AppIndex.jsx
+++ src/componentes/App/AppIndex.jsx
@@ -1,24 +1,36 @@
 import LoggingComponent from "../Loggin/loggin.component";
+import HeaderComponent from "../commons/header/header.component";
 import React from "react";
 export default function AppIndex(){
     const [username, setUsername] = React.useState('');
 
     React.useEffect(()=>{
-        window.localStorage.setItem('username',username);
+        const storedUsername = window.localStorage.getItem('username');
+        if (storedUsername) {
+            setUsername(storedUsername);
+        }
     },[username]);
+
+    React.useEffect(() => {
+        if (username) {
+            window.localStorage.setItem('username', username);
+        }
+    }, [username]);
+    
 
     function updateUsername(value){
         setUsername(value);
     }
+
 
     return (
         <div>
 
         {
             username?(
-                <h1>Hola</h1>
+                <HeaderComponent username = {username}/>
             ):(
-            <LoggingComponent updateUsername={updateUsername} />
+                <LoggingComponent updateUsername={updateUsername} />
             )
         }
 
--- src/componentes/commons/header/header.component.jsx
+++ src/componentes/commons/header/header.component.jsx
@@ -1,8 +1,21 @@
-import styles from 'header.style.css'
-export default function header(){
-    return(
-        <div>
+import './header.style.css';
 
-        </div>
+
+export default function HeaderComponent({username}){
+
+    function onExit(){
+        window.localStorage.clear();
+    }
+
+    return(
+        <header className='header'>
+                <h2 className='header__title'>Welcom to Codeable Keep <strong> {username} </strong></h2>
+                <button className='header__button' onClick={()=>{
+                    onExit();
+                }}>
+                    <label htmlFor="">EXIT</label>
+                </button>
+            
+        </header>
     );
 }
